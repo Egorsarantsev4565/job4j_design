853d79eaf1bb80d473ecdcec0b49abd2df7ce3f9
Egor <67380781+Egorsarantsev4565@users.noreply.github.com>
2021-10-12 20:27:19 +0300

    0.2. FileInputStream [#4898 #212846] - 2

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -2,7 +2,7 @@ package ru.job4j.io;
 
 import java.io.FileInputStream;
 
-public class ReadFile {
+public class EvenNumberFile {
     public static void main(String[] args) {
         try (FileInputStream in = new FileInputStream("even.txt")) {
             StringBuilder text = new StringBuilder();
