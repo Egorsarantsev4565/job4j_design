11845fd6f83650b746f86608c3e5554ce78eb486
Egor <67380781+Egorsarantsev4565@users.noreply.github.com>
2021-11-02 16:38:39 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,56 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() throws IOException {
+        try (BufferedReader in = new BufferedReader(new FileReader(this.path))) {
+            in.lines()
+                    .filter(s -> !(s.startsWith("#") || s.isEmpty()))
+                    .map(s -> s.split("="))
+                    .forEach((strings -> {
+                        if (strings.length != 2) {
+                            throw new IllegalArgumentException();
+                        }
+                        values.put(strings[0], strings[1]);
+                    }));
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                new Config(
+                        "C:/Users/Egor/IdeaProjects/job4j_design/ app.properties.txt"));
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+import java.io.IOException;
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        Config config = new Config(
+                "C:/Users/Egor/IdeaProjects/job4j_design/ app.properties.txt");
+        try {
+            config.load();
+        } catch (IOException e) {
+            fail();
+        }
+        assertThat(
+                config.value("hibernate.connection.url"),
+                is("jdbc:postgresql://127.0.0.1:5432/trackstudio")
+        );
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        Config config = new Config(
+                "C:/Users/Egor/IdeaProjects/job4j_design/nocomment.properties.txt");
+
+        try {
+            config.load();
+        } catch (IOException e) {
+            fail();
+        }
+        assertNull(config.value("hibernate.connection.password=password"));
+    }
+
+    @Test
+    public void whenPairWithourPair() {
+        Config config = new Config(
+                "C:/Users/Egor/IdeaProjects/job4j_design/nopair.properties.txt");
+        try {
+            config.load();
+        } catch (IOException e) {
+            fail();
+        }
+        assertNull(config.value("5"));
+    }
+}
