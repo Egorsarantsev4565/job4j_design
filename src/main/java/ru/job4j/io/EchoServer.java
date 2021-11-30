e7e992568b3c5f7b742863f6b2c2b00f4cce54bb
Egor <egorsarantsev4565@yandex.ru>
2021-11-30 20:35:04 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,8 +3,6 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -14,23 +12,30 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    List<String> msg = new ArrayList<>();
+                    StringBuilder msg = new StringBuilder();
                     String message = in.readLine();
                     while (!message.isEmpty()) {
-                        msg.add(message);
+                        msg.append(message);
                         System.out.println(message);
                         message = in.readLine();
                     }
-                    for (String rsl : msg) {
-                        if (rsl.contains("Bye")) {
-                            out.write("HTTP/1.1 500 STOP\r\n\r\n".getBytes());
-                            out.flush();
-                            server.close();
-                        }
+                    if (msg.indexOf("Exit") != -1) {
+                        out.write("HTTP/1.1 500 STOP\r\n\r\n".getBytes());
+                        out.write("Bye ".getBytes());
+                        out.flush();
+                        server.close();
+                    } else if (msg.indexOf("Hello") != -1) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        out.write("Hello ".getBytes());
+                        out.flush();
+                    } else if (msg.indexOf("Any") != -1) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        out.write("What? ".getBytes());
+                        out.flush();
+                    } else {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        out.flush();
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    out.flush();
-
                 }
             }
         }
