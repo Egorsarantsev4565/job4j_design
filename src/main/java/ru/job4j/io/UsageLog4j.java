e60942955119344727be299831804aaf79d26e84
Egor <egorsarantsev4565@yandex.ru>
2021-11-30 22:05:09 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,18 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Egor Sarantsev";
+        double daysToNewYear = 32.0;
+        char sex = 'm';
+        boolean alive = true;
+        short age = 20;
+        int year = 2021;
+        long day = 30;
+        byte month = 11;
+
+        LOG.debug("User info name : {}, age : {}, sex : {}, alive : {}",
+                name, age, sex, alive);
+        LOG.debug("Date info day : {}, month : {}, year : {}, days to New Year : {}",
+                day, month, year, daysToNewYear);
     }
 }
