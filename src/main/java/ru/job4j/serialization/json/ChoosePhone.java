4f530857766151fbce5c56919f007082ea9fc229
Egor <egorsarantsev4565@yandex.ru>
2021-12-03 17:27:22 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/ru/job4j/serialization/json/ChoosePhone.java
+++ src/main/java/ru/job4j/serialization/json/ChoosePhone.java
@@ -2,6 +2,7 @@ package ru.job4j.serialization.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONObject;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -14,6 +15,7 @@ import java.util.Arrays;
 @XmlAccessorType(XmlAccessType.FIELD)
 
 public class ChoosePhone {
+
     @XmlAttribute
     private boolean available;
     private int price;
@@ -47,6 +49,26 @@ public class ChoosePhone {
                 + '}';
     }
 
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getCollector() {
+        return collector;
+    }
+
+    public Phone getPhone() {
+        return phone;
+    }
+
+    public String[] getAccessories() {
+        return accessories;
+    }
+
     public static void main(String[] args) throws JAXBException {
          ChoosePhone choosePhone = new ChoosePhone(true, 55000, "Mvideo",
                 new String[]{"Charger", "Adapter", "Headphones"},
@@ -68,5 +90,17 @@ public class ChoosePhone {
         } catch (Exception e) {
 
         }
+        JSONObject jsonPhone = new JSONObject("{\"phone\":"
+                + "{\"broken\":\"false\",\"memory\":\"64\","
+                + "\"model\":\"Iphone 7+\"}}");
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("available", choosePhone.isAvailable());
+        jsonObject.put("price", choosePhone.getPrice());
+        jsonObject.put("collector", choosePhone.getCollector());
+        jsonObject.put("phone", choosePhone.getPhone());
+        jsonObject.put("accessories", choosePhone.getAccessories());
+        System.out.println(jsonPhone.toString());
+        System.out.println(jsonObject.toString());
+        System.out.println(new JSONObject(choosePhone).toString());
     }
 }
